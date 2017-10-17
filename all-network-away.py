60c9c378f8bcf9fde1ef19b7120c630d30f4d239
Maximilian Haye <MHajoha@gmail.com>
2017-10-17 21:41:10 +0200

    Complete script, update to 1.0-final
    Add handling of back command.
    Add a few docstrings.
    Add check that the user is (not yet) marked away before (un)marking them away.
    Fix recursive loop caused by handling of self-generated away and back commands.

--- all-network-away.py
+++ all-network-away.py
@@ -13,13 +13,15 @@ https://hexchat.github.io/
 import hexchat
 
 __module_name__ = "All Network Away"
-__module_version__ = "0.1-initial"
+__module_version__ = "1.0-final"
 __module_description__ = "Marks you away on all networks you are connected to when you do it for one"
 
 flag = False
+""":var: Used to keep from handling away commands we generated ourselves, which would do bad things."""
 
 
 def on_away(word, word_eol, userdata):
+    """Hook function to be called upon usage of the away command"""
     global flag
 
     if not flag:
@@ -29,10 +31,31 @@ def on_away(word, word_eol, userdata):
             away_msg = hexchat.get_prefs("away_reason")
 
         for channel in hexchat.get_list("channels"):
-            if channel.type == 1:
+            if channel.type == 1 and channel.context.get_info("away") is None:
                 flag = True
                 channel.context.command("away " + away_msg)
 
         return hexchat.EAT_HEXCHAT
+    else:
+        flag = False
+
+
+def on_back(word, word_eol, userdata):
+    """Hook function to be called upon usage of the back command"""
+    global flag
+
+    if not flag:
+        for channel in hexchat.get_list("channels"):
+            if channel.type == 1 and channel.context.get_info("away") is not None:
+                flag = True
+                channel.context.command("back")
+
+        return hexchat.EAT_HEXCHAT
+    else:
+        flag = False
+
 
 hexchat.hook_command("AWAY", on_away)
+hexchat.hook_command("BACK", on_back)
+
+print("All Network Away loaded.")
